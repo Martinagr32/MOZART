041ca072edc93d90c880045377969210f02f95af
MartínAGR <56026685+Martinagr32@users.noreply.github.com>
2021-03-22 12:56:54 +0100

    docker.py fixed

--- docker/docker.py
+++ docker/docker.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import os
 import webbrowser
 
@@ -11,18 +9,4 @@ if __name__ == "__main__":
 
     os.system('docker run --name ' + containerName + ' -p ' + localPort + ':80 -d ' + image)
 
-=======
-
-import os
-import webbrowser
-
-if __name__ == "__main__":
-
-    image = 'wordpress' # Imagen que se despliega
-    containerName = 'nameWordpress' # Nombre para trabajar mas comodo con el contenedor
-    localPort = '8080' # Puerto local en el que se despliega
-
-    os.system('docker run --name ' + containerName + ' -p ' + localPort + ':80 -d ' + image)
-
->>>>>>> 0b7c0fa52338eda1a50f5154f38b9bbdda518d67
     webbrowser.open('http://localhost:' + localPort, new=2)
